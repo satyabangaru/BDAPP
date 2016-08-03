6217961477da1f703ef4e65410fe021d29235123
satyabangaru <satyabangaru@gmail.com>
2016-08-03 14:57:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,233 @@
+﻿
+var hostWebUrl;
+var appWebUrl;
+var users = new Array(1);
+$(document).ready(function () {
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', sharePointReady);
+    hostWebUrl = decodeURIComponent(manageQueryStringParameter('SPHostUrl'));
+    appWebUrl = decodeURIComponent(manageQueryStringParameter('SPAppWebUrl'));
+    //Insert method  
+   
+})
+
+function sharePointReady() {
+    context = new SP.ClientContext.get_current();
+    web = context.get_web();
+    getUser().done(function (user) {
+        var schema = {};
+        schema['PrincipalAccountType'] = 'User,DL,SecGroup,SPGroup';
+        schema['SearchPrincipalSource'] = 15;
+        schema['ResolvePrincipalSource'] = 15;
+        schema['AllowMultipleValues'] = false;
+        schema['MaximumEntitySuggestions'] = 50;
+        schema['Width'] = '280px';
+
+        //users = new Array(1);
+        var defaultUser = new Object();
+        defaultUser.AutoFillDisplayText = user.get_title();
+        defaultUser.AutoFillKey = user.get_loginName();
+        defaultUser.Description = user.get_email();
+        defaultUser.DisplayText = user.get_title();
+        defaultUser.EntityType = "User";
+        defaultUser.IsResolved = true;
+        defaultUser.Key = user.get_loginName();
+        defaultUser.Resolved = true;
+        users[0] = defaultUser;
+        SPClientPeoplePicker_InitStandaloneControlWrapper('peoplePickerDiv', users, schema);
+        InsertItemToList(users[0].Key);
+    });
+}
+
+function getUser() {
+    var dfd = $.Deferred(function () {
+        user = web.get_currentUser();
+        context.load(user);
+        context.executeQueryAsync(
+            function () {
+                dfd.resolve(user);
+            }),
+            function () {
+                dfd.reject(args.get_message());
+            };
+    });
+    return dfd.promise();
+}
+
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model  
+//$(document).ready(function () {
+//    hostWebUrl = decodeURIComponent(manageQueryStringParameter('SPHostUrl'));
+//    appWebUrl = decodeURIComponent(manageQueryStringParameter('SPAppWebUrl'));
+//    //Insert method  
+//    InsertItemToList();
+//});
+
+//This function is used to get the hostweb url  
+function manageQueryStringParameter(paramToRetrieve) {
+    var params =
+    document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve) {
+            return singleParam[1];
+        }
+    }
+}
+
+
+//Insert List Item to SP host web  
+function InsertItemToList(loginname) {
+    var ctx = new SP.ClientContext(appWebUrl);//Get the SharePoint Context object based upon the URL  
+    var appCtxSite = new SP.AppContextSite(ctx, hostWebUrl);
+    var web = appCtxSite.get_web(); //Get the Site   
+    var listName = "TestUser";
+    var list = web.get_lists().getByTitle(listName); //Get the List based upon the Title  
+    var listCreationInformation = new SP.ListItemCreationInformation(); //Object for creating Item in the List  
+    var listItem = list.addItem(listCreationInformation);
+    //var userField = this.users.get_id() + ";#" + this.users.get_loginName() + ";" + this.users.get_id() + ";#" + this.users.get_loginName();
+    listItem.set_item("Title", "Title1");
+    this.user = ctx.get_web().ensureUser(loginname);
+    listItem.set_item("AssignedTo", this.user);
+    listItem.update(); //Update the List Item  
+    ctx.load(listItem);
+    //Execute the batch Asynchronously  
+    ctx.executeQueryAsync(
+    Function.createDelegate(this, success),
+    Function.createDelegate(this, fail)
+    );
+}
+
+function success() {
+    alert("Item added successfully");
+}
+
+function fail(sender, args) {
+    alert('Failed to get user name. Error:' + args.get_message());
+}
+//var userField = this.user1.get_id() + ";#" + this.user1.get_loginName() + ";" + this.user2.get_id() + ";#" + this.user2.get_loginName();
+//listItem.set_item('UserField', userField);
+//listItem.Update();
+//list.Update();
+
+
+//// Run your custom code when the DOM is ready.
+//$(document).ready(function () {
+
+//    // Specify the unique ID of the DOM element where the
+//    // picker will render.
+//    initializePeoplePicker('peoplePickerDiv');
+//});
+
+//// Render and initialize the client-side People Picker.
+//function initializePeoplePicker(peoplePickerElementId) {
+
+//    // Create a schema to store picker properties, and set the properties.
+//    var schema = {};
+//    schema['PrincipalAccountType'] = 'User,DL,SecGroup,SPGroup';
+//    schema['SearchPrincipalSource'] = 15;
+//    schema['ResolvePrincipalSource'] = 15;
+//    schema['AllowMultipleValues'] = true;
+//    schema['MaximumEntitySuggestions'] = 50;
+//    schema['Width'] = '280px';
+
+//    // Render and initialize the picker. 
+//    // Pass the ID of the DOM element that contains the picker, an array of initial
+//    // PickerEntity objects to set the picker value, and a schema that defines
+//    // picker properties.
+//    this.SPClientPeoplePicker_InitStandaloneControlWrapper(peoplePickerElementId, null, schema);
+//}
+
+//// Query the picker for user information.
+//function getUserInfo() {
+
+//    // Get the people picker object from the page.
+//    var peoplePicker = this.SPClientPeoplePicker.SPClientPeoplePickerDict.peoplePickerDiv_TopSpan;
+
+//    // Get information about all users.
+//    var users = peoplePicker.GetAllUserInfo();
+//    var userInfo = '';
+//    for (var i = 0; i < users.length; i++) {
+//        var user = users[i];
+//        for (var userProperty in user) {
+//            userInfo += userProperty + ':  ' + user[userProperty] + '<br>';
+//        }
+//    }
+//    $('#resolvedUsers').html(userInfo);
+
+//    // Get user keys.
+//    var keys = peoplePicker.GetAllUserKeys();
+//    $('#userKeys').html(keys);
+
+//    // Get the first user's ID by using the login name.
+//    getUserId(users[0].Key);
+//}
+
+//// Get the user ID.
+//function getUserId(loginName) {
+//    var context = new SP.ClientContext.get_current();
+//    this.user = context.get_web().ensureUser(loginName);
+//    context.load(this.user);
+//    context.executeQueryAsync(
+//         Function.createDelegate(null, ensureUserSuccess),
+//         Function.createDelegate(null, onFail)
+//    );
+//}
+
+//function ensureUserSuccess() {
+//    $('#userId').html(this.user.get_id());
+//}
+
+//function onFail(sender, args) {
+//    alert('Query failed. Error: ' + args.get_message());
+//}
+
+
+
+//'use strict';
+
+//var context = SP.ClientContext.get_current();
+//var user = context.get_web().get_currentUser();
+
+//// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+//$(document).ready(function () {
+
+//    getUserName();
+//   // showDialog();
+//});
+
+//function showDialog() {
+
+//    var optDict = {
+//        width: 800,
+//        height: 500,
+//        url: 'http://www.google.com',
+//        title: "Upload your file"
+//    };
+
+//    try {
+//        SP.UI.ModalDialog.showModalDialog(optDict);
+//    }
+//    catch (err) {
+//        alert(err.message);
+//    }
+
+//    return false;
+//}
+
+//// This function prepares, loads, and then executes a SharePoint query to get the current users information
+//function getUserName() {
+//    context.load(user);
+//    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+//}
+
+//// This function is executed if the above call is successful
+//// It replaces the contents of the 'message' element with the user name
+//function onGetUserNameSuccess() {
+//    $('#message').text('Hello ' + user.get_title());
+//}
+
+//// This function is executed if the above call fails
+//function onGetUserNameFail(sender, args) {
+//    alert('Failed to get user name. Error:' + args.get_message());
+//}
--- Default.aspx
+++ Default.aspx
@@ -1,91 +1,70 @@
 ﻿<%-- The following 4 lines are ASP.NET directives needed when using SharePoint components --%>
-<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" MasterPageFile="~masterurl/default.master" language="C#" %>
-<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
-<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
-<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+
+<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" MasterPageFile="~masterurl/default.master" Language="C#" %>
+
+<%@ Register TagPrefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register TagPrefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
 
 <%-- The markup and script in the following Content element will be placed in the <head> of the page --%>
-<asp:Content ContentPlaceHolderId="PlaceHolderAdditionalPageHead" runat="server">
+<asp:Content ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
     <script type="text/javascript" src="../Scripts/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="../_layouts/15/clienttemplates.js"></script>
+    <script type="text/javascript" src="../_layouts/15/clientforms.js"></script>
+    <script type="text/javascript" src="../_layouts/15/clientpeoplepicker.js"></script>
 
+    <script type="text/javascript" src="../_layouts/15/autofill.js"></script>
+    <script type="text/javascript" src="../_layouts/15/sp.runtime.js"></script>
+    <script type="text/javascript" src="../_layouts/15/sp.js"></script>
+    <script type="text/javascript" src="../_layouts/15/sp.core.js"></script>
     <!-- Add your CSS styles to the following file -->
     <link rel="Stylesheet" type="text/css" href="../Content/App.css" />
 
     <!-- Add your JavaScript to the following file -->
     <script type="text/javascript" src="../Scripts/App.js"></script>
+</asp:Content>
 
-    <!-- The following script runs when the DOM is ready. The inline code uses a SharePoint feature to ensure -->
-    <!-- The SharePoint script file sp.js is loaded and will then execute the sharePointReady() function in App.js -->
-    <script type="text/javascript">
-        $(document).ready(function () {
-            SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () { sharePointReady(); });
-            //ExecuteOrDelayUntilScriptLoaded(useClientContext, "sp.js");
-        });
-    </script>
+<%-- The markup in the following Content element will be placed in the TitleArea of the page --%>
+<asp:Content ContentPlaceHolderID="PlaceHolderPageTitleInTitleArea" runat="server">
+    Page Title
 </asp:Content>
 
 <%-- The markup and script in the following Content element will be placed in the <body> of the page --%>
-<asp:Content ContentPlaceHolderId="PlaceHolderMain" runat="server">
-   <%-- <div>
-        <textarea id="tileArea" style="height:auto;width:auto"></textarea>
-    </div>--%>
-    <div id="defaultDiv">
-        <table width="60%" >
-            <tr style="background-color:lightblue">
-                <td>
-                    <select id="selCat" style="width:100%">
-                        <option></option>
-                    </select>
-                </td>
-                <td>
-                    <select id="selDist" style="width:100%">
-                        <option>
-                        </option>
-                    </select>
-                </td>
-                <td  align="left">
-                    <input id="btnFetch" type="button" value="Fetch" onclick="btnFetch_Click()" />
-                </td>
-                <td  align="right">
-                    
-                    <input id="btnSave" type="button" value="Save" onclick="btnSave_Click()" />
-                    <input id="btnCancel" type="button" value="Cancel"  onclick="btnCancel_Click()" />
-                </td>    
-            </tr>
-        </table>
-    </div>
-    <div>
-        <img  alt="" src ="../Images/WindowsPhoneProgressbar.gif" width="60%" height="40px" id="progress"/>
-        <%--<img  alt="" src ="../Images/WindowsPhoneProgressbar.gif" width="60%" id="progress"/>--%>
-    </div>
-    <div id="resultDiv">
-        <table id="resultTable" border="1" width="60%" style="border-style:double" cellspacing="0" cellpadding="0" > </table>
-    </div>
-
-    <div id="dvHiddenFields">
-       <table width="60%" >
-            <tr>
-                <td>
-                    <textarea id="lblResult" rows="10" cols =" 70" style="width:100%;border:none;color:ActiveCaption" readonly="readonly" ></textarea>
-                    <%--<input id="lblResult" type="text" style="width:100%" readonly="readonly" />--%>
-                </td>
-            </tr>
-            
-            <tr>
-                <td>
-                    <input id="originalValues" type="text" style="width:100%;border:none;visibility:hidden" />
-                </td>
-            </tr>       
-       </table>
-    </div>
-        
-    <%-- 
+<asp:Content ContentPlaceHolderID="PlaceHolderMain" runat="server">    
+    <div id="peoplePickerDiv"></div>
     
-    <script type="text/javascript" src="/_layouts/MicrosoftAjax.js" ></script>
-    <script type="text/javascript" src="/_layouts/SP.debug.js"></script>
-    <script type="text/javascript" src="/_layouts/SP.Core.js" ></script>
-    <script type="text/javascript" src="/_layouts/SP.Runtime.js"></script>
-    <script type="text/javascript" src="/_layouts/SP.js"></script>
 
-    --%>
+    <%--<div>
+        <p id="message">
+            <!-- The following content will be replaced with the user name when you run the app - see App.js -->
+            initializing...
+           
+
+        </p>
+         <div>
+             <label for="ProjectName">Project Name</label>
+            </div>
+        <div>
+            <input type="text" id="projectname" />
+        </div>
+                         <div class="form-group">
+                   <label class="sr-only" for="peoplePickerDiv">Assigned To:</label>
+                   <div id="peoplePickerDiv{{todo.id}}"></div>
+                 </div>
+    </div>--%>
+    <%--<script type="text/javascript">
+        $(document).ready(function () {
+            var html = document.createElement('div');
+            html.innerHTML = '<h1>Hi! Welcome to SharePoint</h1>\
+<a href="#" onclick="SP.UI.ModalDialog.commonModalDialogClose(SP.UI.DialogResult.cancel); return false;">Close Dialog</a>\
+<a href="#" onclick="SP.UI.ModalDialog.commonModalDialogClose(SP.UI.DialogResult.OK); return false;">OK</a>';
+            OpenPopUpPageWithDialogOptions({
+                title: "HTML Content",
+                html: html,
+                dialogReturnValueCallback: function (dialogResult) {
+                    alert(dialogResult); //Add your custom code here.
+                }
+            });
+        });
+    </script>--%>
 </asp:Content>
