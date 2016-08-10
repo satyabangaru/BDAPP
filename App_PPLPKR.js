c27437d7eb22203d3d416755b759b38513c536ef
satyabangaru <satyabangaru@gmail.com>
2016-08-10 12:46:48 +0530

    Add files via upload

---
+++ App_PPLPKR.js
@@ -0,0 +1,112 @@
+﻿var UserId = "";
+
+$(document).ready(function () {
+    initializePeoplePicker('peoplePickerDiv');
+   // GetUser();
+});
+function changeFunc() {
+    var selectBox = document.getElementById("mySelect");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    document.getElementById('TextContent').value = selectedValue;
+}
+function GetUser() {
+
+    var select = document.getElementById("mySelect");
+    var length = select.options.length;
+    for (i = length; i > -1; i--) {
+        select.remove(i);
+    }
+    getProperties(UserId.replace("#", "%23"));
+}
+
+function getPropertyValue(LoginName, selectedValue) {
+    $.ajax({
+        url: getQueryStringParameter('SPAppwebUrl') + "/_api/SP.UserProfiles.PeopleManager/GetUserProfilePropertyFor(accountName=@v,propertyName='" + selectedValue + "')?@v=" + "%27" + LoginName + "%27",
+        type: "GET",
+        headers: { "ACCEPT": "application/json;odata=verbose" },
+        success: function (data) {
+            document.getElementById("mySelect").options[document.getElementById("mySelect").selectedIndex].value = data.d.GetUserProfilePropertyFor;
+        },
+        error: function () {
+            alert("Failed to get customer");
+        }
+    });
+}
+//http://siteurl/_api/SP.UserProfiles.PeopleManager/GetUserProfilePropertyFor(accountName=@v,propertyName='LastName')?@v='domain\username'
+function getProperties(LoginName) {
+    $.ajax({
+        url: getQueryStringParameter('SPAppWebUrl') + "/_api/SP.UserProfiles.PeopleManager/GetPropertiesFor(accountName=@v)?@v='" + LoginName + "'",
+        type: "GET",
+        headers: { "ACCEPT": "application/json;odata=verbose" },
+        success: function (data) {
+            var x = document.getElementById("mySelect");
+            var option;
+            for (var i = 0; i < data.d.UserProfileProperties.results.length; i++) {
+                option = document.createElement("option");
+                option.text = data.d.UserProfileProperties.results[i].Key;
+                option.value = data.d.UserProfileProperties.results[i].Value;
+                x.add(option);
+            }
+        },
+        error: function () {
+            alert("Failed to get customer");
+        }
+    });
+}
+
+function SetCurrentUserProperties() {
+    var clientContext = SP.ClientContext.get_current();
+    var peopleManager = new SP.UserProfiles.PeopleManager(clientContext);
+    var selectBox = document.getElementById("mySelect");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].text;
+    peopleManager.setSingleValueProfileProperty(UserId, selectedValue, document.getElementById('TextContent').value);
+
+    clientContext.executeQueryAsync(function () {
+        getPropertyValue(UserId.replace("#", "%23"), selectedValue);
+        SP.UI.Notify.addNotification("properties updated!", false);
+    }, function (sender, args) {
+        alert(args.get_message());
+    });
+}
+function initializePeoplePicker(peoplePickerElementId) {
+
+    var schema = {};
+    schema['PrincipalAccountType'] = 'User,DL,SecGroup,SPGroup';
+    schema['SearchPrincipalSource'] = 15;
+    schema['ResolvePrincipalSource'] = 15;
+    schema['AllowMultipleValues'] = false;
+    schema['MaximumEntitySuggestions'] = 50;
+    schema['Width'] = '280px';
+    schema['Height'] = '55px';
+
+
+    SPClientPeoplePicker.ShowUserPresence = false;
+
+    SPClientPeoplePicker_InitStandaloneControlWrapper(peoplePickerElementId, null, schema);
+}
+
+function getUserInfo() {
+
+    var peoplePicker = this.SPClientPeoplePicker.SPClientPeoplePickerDict.peoplePickerDiv_TopSpan;
+
+    var users = peoplePicker.GetAllUserInfo();
+    var userInfo = '';
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i];
+        for (var userProperty in user) {
+            userInfo += userProperty + ':  ' + user[userProperty] + '<br>';
+        }
+    }
+    var keys = peoplePicker.GetAllUserKeys();
+    UserId = users[0].Key;
+    GetUser();
+}
+function getQueryStringParameter(param) {
+    var params = document.URL.split("?")[1].split("&");
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == param)
+            return decodeURIComponent(singleParam[1]);
+    }
+    return "";
+}
---
+++ ClientWebPart1_PPLPkr.aspx
@@ -0,0 +1,46 @@
+﻿<%-- The following 4 lines are ASP.NET directives needed when using SharePoint components --%>
+<%@ Page Inherits="Microsoft.SharePoint.WebPartPages.WebPartPage, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" language="C#" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="WebPartPages" Namespace="Microsoft.SharePoint.WebPartPages" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+
+<!-- The following tells SharePoint to allow this page to be hosted in an IFrame -->
+<WebPartPages:AllowFraming runat="server" />
+
+<html>
+	<head>
+		<!-- The following scripts are needed when using the SharePoint object model -->
+		<%--<script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.min.js"></script>--%>
+        <script type="text/javascript" src="../Scripts/jquery-1.10.2.min.js"></script>
+         <script type="text/javascript" src="/_layouts/15/MicrosoftAjax.js"></script>
+		<SharePoint:ScriptLink name="Init.js" runat="server" LoadAfterUI="true" Localizable="false" />
+	    <SharePoint:ScriptLink name="SP.js" runat="server" LoadAfterUI="true" Localizable="false" />
+	    <SharePoint:ScriptLink name="SP.Runtime.js" runat="server" LoadAfterUI="true" Localizable="false" />
+	    <SharePoint:ScriptLink name="SP.UI.Dialog.js" runat="server" LoadAfterUI="true" Localizable="false" />
+	    <SharePoint:ScriptLink name="SP.UserProfiles.js" runat="server" LoadAfterUI="true" Localizable="false" />
+		<script type="text/javascript" src="../Scripts/App.js"></script>
+
+		<!-- Add your CSS styles to the following file -->
+		<link rel="Stylesheet" type="text/css" href="../Content/App.css" />
+
+		
+	</head>
+
+	<body>
+        <script type="text/javascript" src="/_layouts/15/1033/strings.js"></script>
+    <script type="text/javascript" src="/_layouts/15/clienttemplates.js"></script>
+    <script type="text/javascript" src="/_layouts/15/clientforms.js"></script>
+    <script type="text/javascript" src="/_layouts/15/clientpeoplepicker.js"></script>
+    <script type="text/javascript" src="/_layouts/15/autofill.js"></script>
+
+		<div>
+	People Picker<br/>
+	<div id="peoplePickerDiv" >
+
+  	</div>
+	<select id="mySelect" onchange="changeFunc()"></select><input type="button" value="Get Properties" onclick="getUserInfo()"/><br/>
+	<input type="text" id="TextContent"  /> <input type="button" value="Update" onclick="SetCurrentUserProperties()"/>
+ </div>
+
+	</body>
+</html>
